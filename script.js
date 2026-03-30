409b9c4fa4a09ea0012bbb6f7097a450ef406802
hal <shinycelebi@protonmail.com>
2026-03-30 15:39:24 -0400

    Get calculator mostly working

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-// operations
-let operator = "";
-let n1 = 0;
-let n2 = 0;
+const calc = document.querySelector("#calc");
+const display = document.querySelector("#display");
+let calcString = "";
+let dotEnabled = false;
+let operatorEnabled = false;
 
+calc.addEventListener('click', (Event) => {
+  target = Event.target.id;
+  if (target === '1' || target === '2' || target === '3'
+   || target === '4' || target === '5' || target === '6'
+   || target === '7' || target === '8' || target === '9'
+   || target === '0') {
+    updateCalc(target);
+    dotEnabled = true;
+    operatorEnabled = true;
+  } 
+  else if (target === 'dot' && dotEnabled === true) {
+    updateCalc(target);
+    dotEnabled = false;
+  }
+  else if (operatorEnabled === true && target === 'add'
+        || target === 'subtract' || target === 'multiply' || target === 'divide') {  
+    updateCalc(target);
+    operatorEnabled = false;
+  }
+  else if (operatorEnabled === false && target === 'add'
+        || target === 'subtract' || target === 'multiply' || target === 'divide') {  
+    operate(calcString);
+    updateCalc(target);
+    operatorEnabled = true;
+  }
+  else if (target === 'clear') {
+    clearCalc();
+  }
+  else if (target === 'equals') {
+    operate(calcString);
+  }
+});
+
+// operations
 function add(n1, n2) {
   return n1 + n2;
 }
@@ -13,58 +48,60 @@ function multiply(n1, n2) {
   return n1 * n2;
 }
 function divide(n1, n2) {
+  if (n2 == 0) {
+    return "NICE TRY.";
+  }
   return n1 / n2;
 }
 
-function operate(operator, n1, n2) {
-  switch (operator) {
-    case '+':
-      return add(n1, n2);
-    case '-':
-      return subtract(n1, n2);
-    case '*':
-      return multiply(n1, n2);
-    case '/':
-      return divide(n1, n2);
+function operate(str) {
+  if (str.includes('+')) {
+    let nums = str.split('+');
+    calcString = add(parseFloat(nums[0], 100), parseFloat(nums[1]));
+    display.textContent = calcString;
   }
+  else if (str.includes('-')) {
+    let nums = str.split('-');
+    calcString = subtract(parseFloat(nums[0]), parseFloat(nums[1]));
+    display.textContent = calcString;
+  }
+  else if (str.includes('*')) {
+    let nums = str.split('*');
+    calcString = multiply(parseFloat(nums[0]), parseFloat(nums[1]));
+    display.textContent = calcString;
+  }
+  else if (str.includes('/')) {
+    let nums = calcString.split('/');
+    calcString = divide(parseFloat(nums[0]), parseFloat(nums[1]));
+    display.textContent = calcString;
+  }   
 }
 
-const calc = document.querySelector("#calc");
-const display = document.querySelector("#display");
-
-calc.addEventListener('click', (Event) => {
-  target = Event.target.id;
-  console.log(target);
-  if (target === '1' || target === '2' || target === '3'
-   || target === '4' || target === '5' || target === '6'
-   || target === '7' || target === '8' || target === '9'
-   || target === '0' || target === 'dot') {
-      updateNumbers(target);
-    } 
-  else if (target === 'add' || target === 'subtract'
-        || target === 'multiply' || target === 'divide') {
-      updateOperator(target);
+function updateCalc(input) {
+  if (calcString.length != 31) {
+    switch (input) {
+      case 'add':
+        input = '+';
+        break;
+      case 'subtract':
+        input = '-';
+        break;
+      case 'multiply':
+        input = '*';
+        break; 
+      case 'divide':
+        input = '/';
+        break; 
+      case 'dot':
+        input = '.';
+        break;
+    }
+    calcString += input;
+    display.textContent = calcString;
   }
-  else if (target === 'clear') {
-    clearCalc();
-  }
-  else if (target === 'equals') {
-    operate(n1, n2, operator);
-  }
-});
-
-function updateNumbers(number) {
-
-}
-
-function updateOperator(operator) {
-
 }
 
 function clearCalc() {
-
-}
-
-function operate(n1, n2, operator) {
-
+  calcString = "";
+  display.textContent = calcString;
 }
